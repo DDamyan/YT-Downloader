b89a0f5cbb5db1b62771a35dd72cb2ea5b71366f
DDamyan <ddminedd@gmail.com>
2021-03-04 13:12:29 +0100

    server /download
    fixed ffmpeg
     => not completely ready

--- server/AppFunctions/download.js
+++ server/AppFunctions/download.js
@@ -1,8 +1,11 @@
-const {filterFormats} = require('ytdl-core');
 const ytdl = require('ytdl-core');
 const {validateLink} = require('../AppFunctions/functions');
 
-exports.download = function (req, res) {
+const ffmpeg = require('ffmpeg-static');
+const cp = require('child_process');
+const fs = require('fs');
+
+/*exports.download = function (req, res) {
   const URL = req.query.url;
   var name = req.query.name;
 
@@ -15,4 +18,103 @@ exports.download = function (req, res) {
     filter: format =>
       format.hasAudio == true && format.hasVideo == false && format.container == 'mp4',
   }).pipe(res); //{format: 'mp3'}
+};*/
+
+exports.download = function (req, res) {
+  req.setTimeout(600000); // 10 minuten
+
+  const ref = validateLink(req.query.url);
+  if (ref) {
+    const userVideoName = req.query.name;
+    if (userVideoName) {
+      const itag = req.query.itag;
+      if (itag) {
+        try {
+          ytdl
+            .getInfo(ref)
+            .then(info => {
+              const dt = new Date().getTime();
+              const name = dt + userVideoName + '.mp4';
+
+              const Wformat = ytdl.chooseFormat(info.formats, {quality: itag});
+              //const result = ytdl.downloadFromInfo(info, {filter: format => format === Wformat});
+
+              //const ref = 'https://www.youtube.com/watch?v=XXYlFuWEuKI';
+              const audio = ytdl.downloadFromInfo(info, {quality: 'highestaudio'});
+              const video = ytdl.downloadFromInfo(info, {filter: format => format === Wformat});
+              console.log('requesting...');
+              const testPRO = cp.spawn(
+                ffmpeg,
+                [
+                  //hide logs on console
+                  '-loglevel',
+                  '8',
+                  '-hide_banner',
+                  // // // //
+                  '-y', // replace existing file
+                  // set using threads
+                  '-threads',
+                  '6',
+                  // Set inputs
+                  '-i',
+                  'pipe:4',
+                  '-i',
+                  'pipe:5',
+                  // Map audio & video from streams
+                  '-map',
+                  '0:a',
+                  '-map',
+                  '1:v',
+                  // Keep encoding
+                  '-c:v',
+                  'copy',
+                  // compressing
+                  '-vcodec',
+                  'libx265',
+                  '-crf',
+                  '50',
+                  // Define output file
+                  name,
+                ],
+                {
+                  windowsHide: true, //true
+                  stdio: [
+                    /* Standard: stdin, stdout, stderr */
+                    'inherit',
+                    'inherit',
+                    'inherit',
+                    /* Custom: pipe:3, pipe:4, pipe:5 */
+                    'pipe',
+                    'pipe',
+                    'pipe',
+                  ],
+                },
+              );
+              testPRO.on('close', () => {
+                console.log('done!');
+
+                res.download(name, err => {
+                  if (err) throw err;
+
+                  fs.unlinkSync(name);
+                });
+              });
+
+              //res.download('./TEST_output (1).mp4', err => console.log('Download failed: ', err));
+              audio.pipe(testPRO.stdio[4]);
+              video.pipe(testPRO.stdio[5]);
+            })
+            .catch(error => res.json({error: error.toString()}));
+        } catch (error) {
+          res.json({error: error.toString()});
+        }
+      } else {
+        res.json({error: 'itag is required'});
+      }
+    } else {
+      res.json({error: 'file name is required'});
+    }
+  } else {
+    res.json({error: 'invalid url'});
+  }
 };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const {info} = require('./AppFunctions/info');
 const {download} = require('./AppFunctions/download');
 
-const ffmpeg = require('ffmpeg-static');
-const cp = require('child_process');
+// const ffmpeg = require('ffmpeg-static');
+// const cp = require('child_process');
+// const fs = require('fs');
 const ytdl = require('ytdl-core');
 
 const express = require('express');
@@ -20,64 +21,20 @@ app.get('/download', download);
 
 app.get('/info', info);
 
-app.get('/test', (req, res) => {
-  try {
-    const ref = 'https://www.youtube.com/watch?v=gpLJKNPtKB8';
-    const audio = ytdl(ref, {quality: 'highestaudio'});
-    const video = ytdl(ref, {quality: 'highestvideo'});
-    console.log('requesting...');
-    const testPRO = cp.spawn(
-      ffmpeg,
-      [
-        //hide logs on console
-        '-loglevel',
-        '8',
-        '-hide_banner',
-        // // // //
-        '-y', // replace existing file
-        // set using threads
-        '-threads',
-        '6',
-        // Set inputs
-        '-i',
-        'pipe:4',
-        '-i',
-        'pipe:5',
-        // Map audio & video from streams
-        '-map',
-        '0:a',
-        '-map',
-        '1:v',
-        // Keep encoding
-        '-c:v',
-        'copy',
-        // Define output file
-        'TEST.mp4',
-      ],
-      {
-        windowsHide: true, //true
-        stdio: [
-          /* Standard: stdin, stdout, stderr */
-          'inherit',
-          'inherit',
-          'inherit',
-          /* Custom: pipe:3, pipe:4, pipe:5 */
-          'pipe',
-          'pipe',
-          'pipe',
-        ],
-      },
-    );
-    testPRO.on('close', () => {
-      console.log('done!');
-    });
-
-    //res.download('./TEST_output (1).mp4', err => console.log('Download failed: ', err));
-    audio.pipe(testPRO.stdio[4]);
-    video.pipe(testPRO.stdio[5]);
-  } catch (error) {
-    console.log('!-error:', error);
-  }
+app.get('/lol', (req, res) => {
+  const ref = 'https://www.youtube.com/watch?v=RUQl6YcMalg';
+  ytdl
+    .getInfo(ref)
+    .then(info => {
+      try {
+        const Wformat = ytdl.chooseFormat(info.formats, {quality: '7'});
+        const result = ytdl.downloadFromInfo(info, {filter: format => format === Wformat});
+        res.json({format: result});
+      } catch (error) {
+        res.json({error: error.toString()});
+      }
+    })
+    .catch(error => res.json({error: error.toString()}));
 });
 
 app.listen(PORT, () => {
